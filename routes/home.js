54ed88d4050465770d7fac55a1a3d0be8bb8e3bc
Ethan Smith <edhsmith@icloud.com>
2018-01-26 10:48:41 +1300

    Restructuring redirects on routes

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,37 @@
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const db = require('../db')
 
 const router = express.Router()
 
+router.use(bodyParser.urlencoded({ extended: true }))
+
+// 
+// Home Functions 
+// 
+
 router.get('/', (req, res) => {
-      res.render('index')
-    })
+  res.render('index')
+})
 //     .catch(err => {
 //       res.status(500).send('DATABASE ERROR: ' + err.message)
 //     })
 // })
+
+router.post ('/', (req, res) => {
+  console.log(req.body)
+  db.getTaggedDabs()
+    .then(generatedDabs => {
+      console.log(generatedDabs)
+      res.render('dabs', {generatedDabs})
+    })
+})
+
+// 
+// End of Home Functions 
+// 
 
 router.get('/dabs', (req, res) => {
   db.getTaggedDabs()
