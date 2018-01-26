69f7c1e6ca110517e762e1e7e728212ce52f0932
Adam Kuhn <adam.kuhn20@gmail.com>
2018-01-26 15:21:55 +1300

    able to add a dab with multiple tags

--- routes/add-dab.js
+++ routes/add-dab.js
@@ -10,4 +10,17 @@ router.get('/', (req, res) => {
       res.render('add-dab')
     })
 
+router.post('/', (req, res) => {
+  const addDab = req.body
+  console.log(addDab)
+  db.newDab(addDab)
+  .then(() => {
+    res.redirect('/addDab')
+})
+})
+
+function showError (err, res) {
+  res.status(500).send('Database Error: ' + err.message)
+}
+
 module.exports = router
